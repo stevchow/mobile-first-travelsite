ef5924ebf0d4273bb6bbc6637a2e285ace1a373d
stevchow <stephen_chow97@yahoo.co.id>
2018-02-21 16:22:56 +0700

    BEM

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,37 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+var gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  cssvars = require("postcss-simple-vars"),
+  nested = require("postcss-nested"),
+  cssImport = require("postcss-import");
 
-gulp.task('default', function() {
+gulp.task("default", function() {
   console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('html', function() {
+gulp.task("html", function() {
   console.log("Imagine something useful being done to your HTML here.");
 });
 
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
+gulp.task("styles", function() {
+  return gulp
+    .src("./app/assets/styles/styles.css")
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .on("error", function(err) {
+      console.log(err.toString());
+
+      this.emit("end");
+    })
+    .pipe(gulp.dest("./app/temp/styles"));
 });
 
-gulp.task('watch', function() {
-
-  watch('./app/index.html', function() {
-    gulp.start('html');
+gulp.task("watch", function() {
+  watch("./app/index.html", function() {
+    gulp.start("html");
   });
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+  watch("./app/assets/styles/**/*.css", function() {
+    gulp.start("styles");
   });
-
 });
