2f0f61988acb59ca47d6917610938a8c9d1e3d88
stevchow <stephen_chow97@yahoo.co.id>
2018-02-21 19:00:24 +0700

    organize gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,11 @@
-var gulp = require("gulp"),
-  watch = require("gulp-watch"),
-  postcss = require("gulp-postcss"),
-  autoprefixer = require("autoprefixer"),
-  cssvars = require("postcss-simple-vars"),
-  nested = require("postcss-nested"),
-  cssImport = require("postcss-import");
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task("default", function() {
-  console.log("Hooray - you created a Gulp task.");
-});
+// gulp.task("default", function() {
+//   console.log("Hoorayy");
+// });
 
-gulp.task("html", function() {
-  console.log("Imagine something useful being done to your HTML here.");
-});
+// gulp.task("html", function() {
+//   console.log("HTML");
+// });
 
-gulp.task("styles", function() {
-  return gulp
-    .src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .on("error", function(err) {
-      console.log(err.toString());
-
-      this.emit("end");
-    })
-    .pipe(gulp.dest("./app/temp/styles"));
-});
-
-gulp.task("watch", function() {
-  watch("./app/index.html", function() {
-    gulp.start("html");
-  });
-
-  watch("./app/assets/styles/**/*.css", function() {
-    gulp.start("styles");
-  });
-});
