35c7b48c25dae7def246f7ea2afc9e8ab96c208d
stevchow <stephen_chow97@yahoo.co.id>
2018-02-27 20:38:01 +0700

    completed paradise

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
+require('./gulp/tasks/sprites');
+require('./gulp/tasks/scripts');
+require('./gulp/tasks/modernizr');
+require('./gulp/tasks/build');
 
-// gulp.task("default", function() {
-//   console.log("Hoorayy");
-// });
 
-// gulp.task("html", function() {
-//   console.log("HTML");
-// });
+
 
